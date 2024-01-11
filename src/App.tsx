4a65346a3ba2ebb6e1f643f0ce00346a329c3d09
adamassad <damssad@outlook.com>
2024-01-11 20:28:44 +0900

    intial2

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,49 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
+    <><!DOCTYPE html>
+    <html>
+      <head>
+        <meta charset="utf-8">
+        <title>My test page</title>
+      </head>
+      <style>
+      .container {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+      }
+      
+      img {
+    
+        max-height:15%;
+        float: left;
+      }
+      
+      .text {
+        font-size: 20px;
+        float: left;
+     padding-left: 20px;
+      }
+      </style>
+    
+    <a href="https://www.homepage.com>
+    <h4><div title="Hover doesnt work">&#128308;AdamAssad</div></h4></a>
+     <div class="container">
+          <div class="image">
+      <body>
+    <img src="C:\Users\damss\Desktop\New folder\shape.png" alt="image" width=300 height=300>
+    </div>
+    <div class="text"> 
+    <h1 style="color: red;">My Heading</h1><br>
+    <p>this is a test  fsdadsfasdfsadf<br>
+    sdfdsfsadfdsafsdadf &#x1F4BB;<br>
+    fsdafasdfsdafsadfasdfadsf.</p> </div>
+        </div>
+     <hr>
+    <h1>&#128308;<b>kia</b></h1>
+      </body>
+    </html>
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
