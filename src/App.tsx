6eb77c65371c263af90fe507f5f5640a39b8b7e8
adamassad <damssad@outlook.com>
2024-01-11 23:50:44 +0900

    a

--- src/App.tsx
+++ src/App.tsx
@@ -2,27 +2,8 @@ import './App.css'
 
 function App() {
   return (
-    <>
-      <a href="https://www.homepage.com">
-        <h4>
-          <div title="Hover doesnt work" >&#128308; AdamAssad</div>
-        </h4>
-      </a>
-      <body>
-        <div className="container">
-          <div className="image" >
-            <img src="shape.png" alt="image" style={{ height: 300, width: 300 }} />
-          </div>
-          <div className="text" >
-            <h1 style={{ color: 'red' }} > My Heading </h1>
-            <br />
-            <p> this is a test  fsdadsfasdfsadf<br />
-              sdfdsfsadfdsafsdadf  💻 <br />
-              fsdafasdfsdafsadfasdfadsf. </p>
-          </div>
-        </div>
-        <hr />
-        <h1>&#128308; <b>kia </b></h1 >
+    <><body>
+<img src="3Line.jpg" alt="Lines"></img>
       </body>
     </>
   )
